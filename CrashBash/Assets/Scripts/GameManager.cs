436b1bbb3d132c8e6d58e23c5ac39f3e9049fd4e
jedineeson <1810027@bart.ca>
2019-02-05 12:04:39 -0500

    First Commit

---
+++ CrashBash/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager m_Instance;
+    public static GameManager Instance
+    {
+        get { return m_Instance; }
+    }
+
+    private void Awake()
+    {
+        if (m_Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            m_Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
