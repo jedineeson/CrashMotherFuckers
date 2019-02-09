92c60db793d800994976a2c36ea6689d96668fca
jedineeson <1810027@bart.ca>
2019-02-09 01:34:03 -0500

    4 controllers Proto Map Cool Player

--- CrashBash/Assets/Scripts/PlayerController.cs
+++ CrashBash/Assets/Scripts/PlayerController.cs
@@ -4,38 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
     private Transform m_MinPos;
-
-    [SerializeField]
     private Transform m_MaxPos;
 
     [SerializeField]
     private float m_MoveSpeed;
 
+    private int m_Index;
+
     private float m_PositionFloat = 0.5f;
 
     private void Update()
     {
-        GetInput();
+        Move();
 
         transform.position = Vector3.Lerp(m_MinPos.position, m_MaxPos.position, m_PositionFloat);
     }
 
-    private void GetInput()
+    private void Move()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
-        {
-            return;
-        }
-        else if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            m_PositionFloat -= m_MoveSpeed * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            m_PositionFloat += m_MoveSpeed * Time.deltaTime;
-        }
+        m_PositionFloat += (m_MoveSpeed * Time.deltaTime) * Input.GetAxis("DirectionP" + m_Index);
+
         m_PositionFloat = Mathf.Clamp(m_PositionFloat, 0f, 1f);
+    }
+
+    public void SetController(Transform minPos, Transform maxPos, int index)
+    {
+        m_MinPos = minPos;
+        m_MaxPos = maxPos;
+        m_Index = index;
     }
 }
---
+++ CrashBash/Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> m_Prefabs;
+
+    [SerializeField]
+    private List<Transform> m_SpawnPos;
+
+    [SerializeField]
+    private List<Transform> m_MinPos;
+    [SerializeField]
+    private List<Transform> m_MaxPos;
+
+    private void Start()
+    {
+        PlayerController controller;
+        for (int i = 0; i < m_Prefabs.Count; i++)
+        {
+            controller = Instantiate(m_Prefabs[i], m_SpawnPos[i].position, m_SpawnPos[i].rotation ).GetComponent<PlayerController>();
+            controller.SetController(m_MinPos[i], m_MaxPos[i], i + 1);
+        }
+    }
+}
