fd16c08be237eb8a9757b8814cac61281e14a85b
patrenaud <pat_renaud@hotmail.com>
2019-02-11 11:48:22 -0500

    ballscript

---
+++ CrashBash/Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private float m_MoveSpeed = 1;
+
+ 
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void SelfDestruct()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ CrashBash/Assets/Scripts/BallInstantiator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallInstantiator : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
