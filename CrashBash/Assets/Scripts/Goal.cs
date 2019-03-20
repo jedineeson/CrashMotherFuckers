4bc3727196e7fe29ff1a282609011bf91caf99b9
jedineeson <1810027@bart.ca>
2019-03-20 12:28:27 -0400

    yeahyeah

--- CrashBash/Assets/Scripts/Goal.cs
+++ CrashBash/Assets/Scripts/Goal.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    private int m_Score = 20;
+    [SerializeField]
+    private GameObject m_Wall;
+
+    private int m_Score = 3;
 
     private PlayerController m_Controller;
     public PlayerController Controller
     {
         set { m_Controller = value; }
+    }
+
+    private TextMeshProUGUI m_ScoreUI;
+    public TextMeshProUGUI ScoreUI
+    {
+        set { m_ScoreUI = value; }
+    }
+
+    private void Start()
+    {
+        m_Wall.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider aOther)
@@ -18,11 +33,18 @@ public class Goal : MonoBehaviour
         {
             aOther.gameObject.GetComponent<Ball>().SelfDestruct();
             m_Score--;
+            UpdateUI();
             if (m_Score <= 0)
             {
-                //
+                Destroy(m_Controller.gameObject);
+                m_Wall.SetActive(true);
             }
         }
+    }
+
+    public void UpdateUI()
+    {
+        m_ScoreUI.text = m_Score.ToString();
     }
 
     private void Update()
